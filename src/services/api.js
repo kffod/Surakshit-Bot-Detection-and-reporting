565904d1f3bc1581f702407e291ebf0eea566404
Sachin Jadhav <98410437+kffod@users.noreply.github.com>
2025-05-28 01:39:58 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://surakshit-bot-detection-and-reporting.onrender.com/api';
 
 export const api = {
   predict: async (screenName) => {
